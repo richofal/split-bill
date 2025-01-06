3c8b44fc7839cccba954fadd716f1fa6a49d1d5b
Richo Febrian <richofal25@gmail.com>
2025-01-06 15:09:18 +0700

    Menampilkan dan menyembunyikan form tambah teman

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,25 @@
+import { useState } from "react";
 import initialFriends from "./data/initialFriends";
 import FriendList from "./components/FriendList";
 import FormAddFriend from "./components/FormAddFriend";
 import FormSplitBill from "./components/FormSplitBill";
 
 function App() {
+  const [showAddFriend, setShowAddFriend] = useState(false);
+  function handleShowAddFriend() {
+    setShowAddFriend((showAddFriend) => !showAddFriend);
+  }
   return (
     <div className="min-h-[66vh] grid grid-cols-[34rem_44rem] gap-x-[4rem] items-start">
       <div>
         <FriendList friends={initialFriends} />
-        <FormAddFriend />
+        {showAddFriend && <FormAddFriend />}
         <button
           className="bg-[var(--color-medium)] text-[#393f44] py-[0.8rem] px-[1.2rem] border-none rounded-[7px] font-bold 
       cursor-pointer transition duration-300 hover:bg-[var(--color-dark)] hover:text-[#dde3e7] float-right mr-[1.2rem]"
+          onClick={handleShowAddFriend}
         >
-          Tambah Teman
+          {showAddFriend ? "Tutup" : "Tambah Teman"}
         </button>
       </div>
       <FormSplitBill />
